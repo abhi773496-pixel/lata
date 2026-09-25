694f7258c1ead39b9987a6b94b8ec671ccd25905
abhi773496-pixel <abhi773496@gmail.com>
2026-09-25 14:52:42 +0530

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,2 +0,0 @@
-import type {Config} from 'tailwindcss';
-const config:Config={content:['./app/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],theme:{extend:{colors:{ivory:'#f7f3ed',charcoal:'#201e1c',silver:'#d7d3cd',champagne:'#c7a77b',blush:'#e9ddd5'}}}};export default config;
